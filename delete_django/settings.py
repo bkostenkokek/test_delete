52d658a34620097b81f5611eb4314342a86cdd59
bkostenkokek <bogdankostenko83@gmail.com>
2023-06-12 18:20:05 +0300

    Docker8

--- delete_django/settings.py
+++ delete_django/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-jvcm50ny1m+u*hv6nk$1cx7^gwokps(3ru(n0r$5n@^$u!2iu6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.67.95.138']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
