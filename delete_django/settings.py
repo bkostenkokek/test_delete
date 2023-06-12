c07ad54a709a2f54921b190354269e95688d6b25
bkostenkokek <bogdankostenko83@gmail.com>
2023-06-12 15:17:13 +0300

    dec211

--- delete_django/settings.py
+++ delete_django/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jvcm50ny1m+u*hv6nk$1cx7^gwokps(3ru(n0r$5n@^$u!2iu6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['3.67.95.138']
 
